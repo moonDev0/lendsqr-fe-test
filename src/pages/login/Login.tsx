fa5ac74aa18c91b9a7db977f416ec42630fb9f79
Grixxley <Idominion271@gmail.com>
2023-06-16 04:36:57 +0100

    added login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,9 +1,54 @@
-import "./login.scss"
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './login.scss';
+import loginsvg from "../../asset/pablo-sign-in 1.svg"
+import logo from "../../asset/Group.svg"
 
-const Login = () => {
+const LoginPage: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    // Perform login logic here
+    // Once logged in, navigate to the dashboard page
+    navigate('/admin');
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login-container">
+     <div className="logo">
+     <img src={logo} alt="" />
+     </div>
+      <div className="login-content">
 
-export default Login
+        <div className="image-container left-box">
+          <img src={loginsvg} alt="Logo" />
+        </div>
+
+        <div className="form-container right-box">
+       
+          <div className="login-form">
+          <h1> Welcome!</h1>
+          <h3>Enter details to login.</h3>
+          {/*  login form fields  */}
+          <br />
+            <div>
+               <input type="email" placeholder="Email" />
+            </div>
+              
+              <br />
+            <div>
+            <input type="password" placeholder="Password" />
+            {/* <button>Show</button> */}
+            </div>
+              <br />
+              <button onClick={handleLogin}>LOG IN</button>
+          </div>
+         
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
